3f8e8a349f693a0399a6c6a7aa14e5df946f7fc8
alfonsocanor <canorluisa@gmail.com>
2021-01-23 20:56:07 -0500

    Updating DockerCompose

--- app.js
+++ app.js
@@ -8,29 +8,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//app.use(express.json()) // for parsing application/json
-
-/*const getRoutes = {
-    hostname: 'http://router.project-osrm.org/table/v1',
-    port: 3000,
-    path: '/driving',
-    method: 'POST'
-}*/
-
-/*app.get('/validateAllLocations', (rep, res) => 
-    res.send('Hello service, from my hearth!'
-));
-   
-app.listen(3000, () => {
-    console.log('route-service on port 3000');
-});*/
-//const geoinfo = require('./api/routes/geoinfo');
 const routing = require('./api/routes/routing');
-/* app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'Route server'
-    });
-}); */
 
 //app.use('/geoinfo', geoinfo);
 app.use('/routing', routing)
